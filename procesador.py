dc08f26798fc2efdb883becec487c3ecc4f1aa26
systemsvcr-11 <systemsvcr@gmail.com>
2026-08-13 11:43:03 -0500

    Update procesador.py

--- procesador.py
+++ procesador.py
@@ -23,8 +23,9 @@ MAPEO_AREAS = {
     },
     "La Guardiana": {
         "COCINA GUARDIANA": "COCINA",
-        "BARRA GUARDIANA": "BARRA",
-        "CAJA GUARDIANA": "CAJA",
+        "Maritza Alcasihuincha": "BARRA",
+        "Maria Villalobos": "CAJA",
+        "Coordinador Guardiana": "ATENCION",
     },
     "Centro de Producción": {
         "PRODUCCION": "PRODUCCION",
